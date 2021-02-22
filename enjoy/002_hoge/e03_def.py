37849874c51b089360c7c23b48ede27baa76b185
fkubota <rkkubotay@gmail.com>
2021-02-22 22:04:16 +0900

    [update] 001_20200224

---
+++ enjoy/002_hoge/e03_def.py
@@ -0,0 +1,47 @@
+'''
+↓にしろ(カッコにa,b,c,dがないので入れろ)
+
+def hoge_a(a):
+    ...
+
+def hoge_b(b):
+    ...
+
+def hoge_c(c):
+    ...
+
+def hoge_d(d):
+    ...
+'''
+
+def hoge_a():
+    print(a)
+    print(a)
+    print(a)
+
+print('hoge')
+print('hoge')
+print('hoge')
+
+def hoge_b():
+    print(b)
+    print(b)
+    print(b)
+
+print('hoge')
+print('hoge')
+print('hoge')
+print('hoge')
+
+def hoge_c():
+    print(c)
+    print(c)
+    print(c)
+
+print('hoge')
+print('hoge')
+
+def hoge_d():
+    print(d)
+    print(d)
+    print(d)
