7796b6015f28c626877488eb607048a74bd100ea
fkubota <rkkubotay@gmail.com>
2021-02-22 21:54:52 +0900

    [update] 001_20200224

---
+++ enjoy/002_hoge/e01_indent.py
@@ -0,0 +1,13 @@
+'''
+↓にしろ
+
+for i in range(10):
+    for j in range(10):
+        print(i, j)
+        print(i+10, j+10)
+'''
+
+for i in range(10):
+    for j in range(10):
+print(i, j)
+print(i+10, j+10)
