7ae49b1121999523595af498ddf720224b534720
fkubota <rkkubotay@gmail.com>
2021-03-05 11:19:12 +0900

    [update]

--- enjoy/002_20200228/live_coding/func.py
+++ enjoy/002_20200228/live_coding/func.py
@@ -1,12 +1,7 @@
-import pandas as pd
 from ipdb import set_trace as st
 from sklearn.datasets import load_iris
 
 
-def get_datasets():
+def load_dataset():
     iris = load_iris()
-    data = iris.data
-    feat_name = iris.feature_names
-    target = iris.target
-    df = pd.DataFrame(data, columns=feat_name)
-    return df, target
+    return iris.data, iris.target
--- enjoy/002_20200228/live_coding/iris.py
+++ enjoy/002_20200228/live_coding/iris.py
@@ -1,33 +1,31 @@
-import yaml
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 from ipdb import set_trace as st
+from func import load_dataset
 import pandas as pd
 import time
-import func
 
 
 def main():
-    print(f'sart: {time.ctime()}')
-    with open('config.yml') as file:
-        config = yaml.safe_load(file)
+    print(f'start {time.ctime()}')
+    data, target = load_dataset()
 
-    model_params = config['model']['params']
+    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=42)
 
-    df, target = func.get_datasets()
-    train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(
-            df, target, test_size=0.33, random_state=42)
 
-    model = RandomForestClassifier(**model_params)
+    # model
+    model = DecisionTreeClassifier(max_depth=2)
     model.fit(X_train, y_train)
-
     y_test_pred = model.predict(X_test)
 
+    # eval
     acc_test = accuracy_score(y_test, y_test_pred)
 
-    print(f'acc_test: {acc_test}')
+    print(f'acc_test = {acc_test}')
+
+
+
 
 
 if __name__ == '__main__':
