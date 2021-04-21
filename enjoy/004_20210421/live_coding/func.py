665ecdda8a8e7ca121585e707f8b814b302af394
fkubota <rkkubotay@gmail.com>
2021-04-21 21:05:48 +0900

    [update]

--- enjoy/004_20210421/live_coding/func.py
+++
@@ -1,8 +0,0 @@
-from sklearn.datasets import load_iris
-import matplotlib.pyplot as plt
-
-
-def load_dataset():
-    iris = load_iris()
-
-    return iris.data, iris.target
--- enjoy/004_20210421/live_coding/iris.py
+++ enjoy/004_20210421/live_coding/iris.py
@@ -1,35 +1,49 @@
 import yaml
-import matplotlib.pyplot as plt
+from ipdb import set_trace as st
+import numpy as np
+import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-import time
-import func
+
+
+def get_datasets():
+    iris = load_iris()
+    data = iris.data
+    target = iris.target
+    feature_names = iris.feature_names
+
+    df = pd.DataFrame(data, columns=feature_names)
+
+    return df, target
+
+
+def get_config():
+    with open('config.yml') as file:
+        config = yaml.safe_load(file)
+    return config
 
 
 def main():
-    print(f'start {time.ctime()}')
-    # config
+    print('hello vim')
+    config = get_config()
+    seed = config['globals']['seed']
+    test_size = config['globals']['test_size']
+    df, target = get_datasets()
 
-    with open('config.yml') as file:
-        config = yaml.safe_load(file)
-
-    data, target = func.load_dataset()
-
-    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=42)
-
+    # train test split
+    X_train, X_test, y_train, y_test = train_test_split(
+            df, target, test_size=test_size, random_state=seed)
     # model
-    model = DecisionTreeClassifier(**config['model']['params'])
+    model = DecisionTreeClassifier(random_state=seed)
     model.fit(X_train, y_train)
 
-    # pred
-    y_test_pred = model.predict(X_test)
-
     # eval
-    acc_test = accuracy_score(y_test, y_test_pred)
+    y_test_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_test_pred)
 
-    print(f'acc_test: {acc_test}')
-    print(f'end {time.ctime()}')
+    print(f'accuracy_test: {acc:.4f}')
 
 
 if __name__ == '__main__':
