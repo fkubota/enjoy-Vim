a534d82dfe3e1cc62ee8cc2e400a916c1552f6f2
fkubota <rkkubotay@gmail.com>
2021-07-31 08:05:24 +0900

    [add] 005

---
+++ enjoy/005_20210731/e03_log.py
@@ -0,0 +1,14 @@
+'''
+↓を書け
+
+n_epoch = 30
+
+for epoch in range(n_epoch):
+    print(f'train_loss: {epoch*1}')
+    print(f'valid_loss: {epoch*2}')
+'''
+
+n_epoch = 30
+
+for epoch in range(n_epoch):
+    print(f'train_loss: {epoch*1}')
